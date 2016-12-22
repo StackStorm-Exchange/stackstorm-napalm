443eda0e41a3d481e09138dccfef310ed06e42fc
Matt Oswalt <oswaltm@brocade.com>
2016-12-21 17:51:43 -0800

    Fixed some arguments-related issues
    Signed-off-by: Matt Oswalt <oswaltm@brocade.com>

--- actions/napalm_loadconfig.py
+++ actions/napalm_loadconfig.py
@@ -9,16 +9,17 @@ class NapalmLoadConfig(Action):
     def __init__(self, *args, **kwargs):
         super(NapalmLoadConfig, self).__init__(*args, **kwargs)
 
-    def run(self, driver, hostname, username, password, port, config, method="merge"):
+    def run(self, driver, hostname, username, password, port, config_file, method):
 
-        # Need to determine the best way to GET the config to put on
-        # the device. You currently just require the text, but maybe they
-        # want to give a file.
-        # Maybe they want to give a git repository.....
-
-        method = method.lower()
-        if method not in ["merge", "replace"]:
-            raise ValueError
+        # Usually I'd rely on setting the "method" arg for this function as an optional arg, but
+        # that doesn't seem to work - I'm guessing the caller for this function is actually calling
+        # this function with method set to "None" if the action is omitting this arg
+        if not method:
+            method = 'merge'
+        else:
+            method = method.lower()
+            if method not in ["merge", "replace"]:
+                raise ValueError
 
         with get_network_driver(driver)(
             hostname=str(hostname),
@@ -29,11 +30,11 @@ class NapalmLoadConfig(Action):
 
             if method == "replace":
                 device.load_replace_candidate(
-                    filename='test/unit/eos/new_good.conf'
+                    filename=config_file
                 )
             else:
                 device.load_merge_candidate(
-                    config='interface Ethernet2\ndescription bla'
+                    filename=config_file
                 )
 
         return "load (%s) successful on %s" % (method, hostname)
