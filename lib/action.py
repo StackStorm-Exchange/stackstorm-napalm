8d41bc8a3fc8b61233132cfdebbac709680ef8c4
Rob Woodward <robwwd@icloud.com>
2017-01-25 11:17:15 +0000

    Slight change to how config is passed

---
+++ lib/action.py
@@ -0,0 +1,29 @@
+from napalm import get_network_driver
+
+from st2actions.runners.pythonrunner import Action
+
+__all__ = [
+    'NapalmBaseAction'
+]
+
+class NapalmBaseAction(Action):
+
+    def __init__(self, config):
+        super(NapalmBaseAction, self).__init__(config=config)
+
+    def _get_credentials(self, credentials):
+
+        authconfig = self.config['credentials'].get(credentials, None)
+
+        if not authconfig:
+            raise ValueError(('Invalid credentials set name "%s". Please make'
+                              ' sure that credentials set with this name is'
+                              ' defined in the config' % (credentials)))
+
+        if authconfig['password'] is None:
+            raise ValueError("Missing password in credentials.")
+
+        if authconfig['username'] is None:
+            raise ValueError("Missing username in credentials.")
+
+        return authconfig
