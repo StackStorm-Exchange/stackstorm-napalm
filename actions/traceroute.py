0dc89ca45fa78105dc157e217078077bfdc28f6a
Rob Woodward <robwwd@icloud.com>
2017-01-26 15:12:48 +0000

    New trace route command

---
+++ actions/traceroute.py
@@ -0,0 +1,31 @@
+from napalm import get_network_driver
+
+from lib.action import NapalmBaseAction
+
+class NapalmTraceroute(NapalmBaseAction):
+
+    def run(self, driver, hostname, port, credentials, destination, source=u'', ttl=255, timeout=2):
+
+        login = self._get_credentials(credentials)
+
+        try:
+
+            if not port:
+                optional_args=None
+            else:
+                optional_args={'port': str(port)}
+
+            with get_network_driver(driver)(
+                hostname=str(hostname),
+                username=login['username'],
+                password=login['password'],
+                optional_args=optional_args
+            ) as device:
+
+                route = device.traceroute(destination, source, ttl, timeout)
+
+        except Exception, e:
+            self.logger.error(str(e))
+            return (False, str(e))
+
+        return (True, route)
