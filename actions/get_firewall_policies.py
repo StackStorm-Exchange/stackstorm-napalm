cd543daba918ad8fa940437dec5529349c261506
Rob Woodward <robwwd@icloud.com>
2017-01-31 11:53:22 +0000

    Including the message in the bgp prefix trigger workflow

--- actions/get_firewall_policies.py
+++ actions/get_firewall_policies.py
@@ -4,37 +4,37 @@ from lib.action import NapalmBaseAction
 
 class NapalmGetFirewallPolicies(NapalmBaseAction):
 
-    def run(self, hostname, driver, port, credentials):
+	def run(self, hostname, driver, port, credentials):
 
-        login = self._get_credentials(credentials)
-        
-        # Look up the driver if it's not given from the configuration file
-        # Also overides the hostname since we might have a partial host i.e. from
-        # syslog such as host1 instead of host1.example.com
-        #
-        if not driver:
-            (hostname, driver) = self.find_device_from_config(hostname)
+		login = self._get_credentials(credentials)
 
-            if not driver:
-                raise ValueError(('Can not find driver for host %s, try with driver parameter.' % (hostname)))
+		# Look up the driver if it's not given from the configuration file
+		# Also overides the hostname since we might have a partial host i.e. from
+		# syslog such as host1 instead of host1.example.com
+		#
+		if not driver:
+			(hostname, driver) = self.find_device_from_config(hostname)
 
-        try:
+			if not driver:
+				raise ValueError(('Can not find driver for host %s, try with driver parameter.' % (hostname)))
 
-            if not port:
-                optional_args=None
-            else:
-                optional_args={'port': str(port)}
+		try:
 
-            with get_network_driver(driver)(
-                hostname=str(hostname),
-                username=login['username'],
-                password=login['password'],
-                optional_args=optional_args
-            ) as device:
-                result = device.get_firewall_policies()
+			if not port:
+				optional_args=None
+			else:
+				optional_args={'port': str(port)}
 
-        except Exception, e:
-            self.logger.error(str(e))
-            return (False, str(e))
+			with get_network_driver(driver)(
+				hostname=str(hostname),
+				username=login['username'],
+				password=login['password'],
+				optional_args=optional_args
+			) as device:
+				result = device.get_firewall_policies()
 
-        return (True, result)
+		except Exception, e:
+			self.logger.error(str(e))
+			return (False, str(e))
+
+		return (True, result)
