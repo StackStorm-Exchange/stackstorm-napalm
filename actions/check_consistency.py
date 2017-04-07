02f768f4ed4b2552ddb9b590ceb68abfe3eca4e7
Matt Oswalt <oswaltm@brocade.com>
2017-04-07 13:16:05 -0700

    Update consistency check action
    Signed-off-by: Matt Oswalt <oswaltm@brocade.com>

--- actions/check_consistency.py
+++ actions/check_consistency.py
@@ -1,11 +1,20 @@
 import difflib
+import logging
 import os
+import re
 import shutil
 import tempfile
 
 from lib.action import NapalmBaseAction
 
 import git
+
+# Suppressing the 'No handlers could be found for logger "st2.st2common.util.loader"' message
+# that appears when including the TempRepo class, until I have a chance to troubleshoot why
+# this is happening TODO(mierdin)
+loaderlog = logging.getLogger('st2.st2common.util.loader')
+loaderlog.setLevel(logging.DEBUG)
+loaderlog.addHandler(logging.NullHandler())
 
 
 class TempRepo(object):
@@ -25,10 +34,16 @@ class NapalmCheckConsistency(NapalmBaseAction):
 
         with TempRepo() as tmpdir:
             repo_path = os.path.join(tmpdir, 'repo')
-            repo = git.Repo.clone_from(repo, repo_path, branch='master')
-            with open('%s/%s.txt' % (repo_path, device)) as config_file:
-                return "".join(config_file.readlines())
+            git.Repo.clone_from(repo, repo_path, branch='master')
 
+            try:
+
+                with open('%s/%s.txt' % (repo_path, device)) as config_file:
+                    return "".join(config_file.readlines())
+
+            except IOError:
+                self.logger.error("Golden config not present in repo")
+                raise
 
     def run(self, repository=None, **std_kwargs):
 
@@ -52,10 +67,23 @@ class NapalmCheckConsistency(NapalmBaseAction):
                 golden_config = self.get_golden_config(repository, self.hostname)
                 actual_config = device.get_config()['running']
 
+                # Regular expressions for matching lines to ignore
+                # Lot of network devices have lines like "last modified" that we don't
+                # want to include in the diff
+                #
+                # In the future, this may be a configurable option, but we're doing
+                # this statically for now.
+                ignore_regexs = [
+                    "## .*\n"
+                ]
+                for pattern in ignore_regexs:
+                    actual_config = re.sub(pattern, "", actual_config)
+                    golden_config = re.sub(pattern, "", golden_config)
+
                 # Generate diff
-                golden=golden_config.splitlines(1)
-                actual=actual_config.splitlines(1)
-                diff=difflib.unified_diff(golden, actual)
+                golden = golden_config.splitlines(1)
+                actual = actual_config.splitlines(1)
+                diff = difflib.unified_diff(golden, actual)
                 diff = ''.join(diff)
 
                 if diff:
