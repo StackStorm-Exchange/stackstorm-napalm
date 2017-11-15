b84ba2873c215ffc778923c850f8706acc2009cf
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-11-15 01:18:11 +0000

    Added a new parameter (config_text) to loadconfig action to be able to specify a command to set to device as text

--- actions/loadconfig.py
+++ actions/loadconfig.py
@@ -1,29 +1,30 @@
 from lib.action import NapalmBaseAction
 
+from napalm.base.exceptions import MergeConfigException
+from napalm.base.exceptions import ReplaceConfigException
+
 
 class NapalmLoadConfig(NapalmBaseAction):
     """Load configuration into network device via NAPALM
     """
 
-    def run(self, config_file, method, inline_transfer, **std_kwargs):
+    def run(self, config_file, config_text, method, inline_transfer, **std_kwargs):
+        try:
+            with self.get_driver(**std_kwargs) as device:
+                # inline_transfer: If set it becomes True, else False
+                device.inline_transfer = inline_transfer
 
-        if not method:
-            method = 'merge'
-        else:
-            method = method.lower()
-            if method not in ["merge", "replace"]:
-                raise ValueError(('{} is not a valid load method, use: '
-                                  'merge or replace').format(method))
+                if method == "replace":
+                    device.load_replace_candidate(filename=config_file, config=config_text)
+                else:
+                    device.load_merge_candidate(filename=config_file, config=config_text)
 
-        with self.get_driver(**std_kwargs) as device:
-            # inline_transfer: If set it becomes True, else False
-            device.inline_transfer = inline_transfer
-
-            if method == "replace":
-                device.load_replace_candidate(filename=config_file)
-            else:
-                device.load_merge_candidate(filename=config_file)
-
-            device.commit_config()
+                device.commit_config()
+        except ValueError as e:
+            return (False, e.message)
+        except ReplaceConfigException as e:
+            return (False, e.message)
+        except MergeConfigException as e:
+            return (False, e.message)
 
         return (True, "load ({}) successful on {}".format(method, self.hostname))
